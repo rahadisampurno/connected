78fc63e52f9ce0179151dde16aa4fa5720971e31
telkom-dev <rahadi.rahasia@gmail.com>
2025-07-01 08:11:40 +0700

    add workflow pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Ini sudah benar untuk repositori 'connected'
+  base: '/connected/', 
+
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
